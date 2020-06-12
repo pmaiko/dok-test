5dd2221af345e86763d52cabfd620cae79ffd708
pmaiko <petyamaiko@gmail.com>
2020-06-12 12:29:05 +0300

    feat(project): base settings project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import './sass/app.scss'
 
 Vue.config.productionTip = false
 
